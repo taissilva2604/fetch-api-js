288411c22b3ee581b2912e844ce7bc1953ab04eb
taissilva2604 <taisinha2604@gmail.com>
2024-08-06 15:36:01 -0300

    cachorro

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-document.getElementById('fetch-users').addEventListener('click', fetchUsers);
-
-function fetchUsers() {
-    fetch('https://randomuser.me/api/?results=20')
+document.getElementById('fetchButton').addEventListener('click', fetchDogImages);
+ 
+function fetchDogImages() {
+    fetch('https://api.thedogapi.com/v1/images/search?limit=10')
         .then(response => response.json())
         .then(data => {
-            const users = data.results;
-            const userContainer = document.getElementById('user-container');
-            userContainer.innerHTML = '';
-            users.forEach(user => {
-                const userCard = document.createElement('div');
-                userCard.className = 'user-card';
-                userCard.innerHTML = `
-                    <img src="${user.picture.medium}" alt="User Picture">
-                    <h3>${user.name.first} ${user.name.last}</h3>
-                    <p>${user.email}</p>
-                    <p>${user.location.city}, ${user.location.country}</p>
-                `;
-                userContainer.appendChild(userCard);
+            const gallery = document.getElementById('gallery');
+            gallery.innerHTML = '';
+            data.forEach(dog => {
+                const img = document.createElement('img')
+                img.src = dog.url;
+                gallery.appendChild(img);
             });
         })
-        .catch(error => console.error('Erro ao buscar usuários:', error));
+        .catch(error => console.error('Error fetching dog images:',));
 }
+ 
+
+    
+
